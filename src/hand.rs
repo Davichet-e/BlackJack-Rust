97d00d10e842de94eaa4c726cd53239738e6beb1
David Garcia <davgarmor2@alum.us.es>
2019-09-15 13:57:36 +0200

    ⛑ Minor changes

--- src/hand.rs
+++ src/hand.rs
@@ -42,41 +42,39 @@ impl Hand {
             aces: 0,
         };
         for card in hand.cards.clone() {
-            hand.check_if_ace(card);
+            hand.check_if_ace(&card);
         }
         hand.check_ace_points();
         hand
     }
+
     pub fn has_blackjack(&self) -> bool {
         self.cards.len() == 2 && self.points == 21
     }
-
     pub fn initialize_attributes(&mut self, deck: &mut Deck) {
         *self = Hand::new(deck);
     }
 
     pub fn deal_card(&mut self, deck: &mut Deck) {
         let card: Card = deck.deal_card();
-        self.check_if_ace(card.clone());
-        self.cards.push(card.clone());
+        self.check_if_ace(&card);
+
+        self.cards.push(card);
         self.update_points();
-        if self.check_if_lost() {
+
+        if self.points > 21 {
             self.points = 0;
         }
     }
 
-    fn check_if_ace(&mut self, card: Card) {
+    fn check_if_ace(&mut self, card: &Card) {
         if card.name == "ACE" {
             self.aces += 1;
         }
     }
 
-    fn check_if_lost(&self) -> bool {
-        self.points > 21
-    }
-
     fn check_ace_points(&mut self) {
-        while self.aces > 0 && self.check_if_lost() {
+        while self.aces > 0 && self.points > 21 {
             self.points -= 10;
             self.aces -= 1;
         }
