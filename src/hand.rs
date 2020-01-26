f0c1cd08784dbb2b3d1f7cb60c22901fc069c837
David Garcia <davidgmorillop@gmail.com>
2020-01-26 19:03:28 +0100

    🔨 Refactor implementation of `fmt::Display` for Hand
    Also added `from_cards` function

--- src/hand.rs
+++ src/hand.rs
@@ -1,37 +1,37 @@
-use crate::{Card, Deck};
+use crate::deck::{Card, Deck};
+
 use std::fmt;
 
 #[derive(Clone)]
 pub struct Hand {
     pub cards: Vec<Card>,
     pub points: u8,
-    pub aces: u8,
+    aces: u8,
 }
 
 impl fmt::Display for Hand {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let points: u8 = self.points;
+        let cards_as_string: Vec<String> = self.cards.iter().map(|card| card.to_string()).collect();
+        let (last, elements): (&String, &[String]) =
+            cards_as_string.split_last().expect("Hand was empty.");
+
         write!(
             f,
-            "{}",
-            self.cards
-                .iter()
-                .map(|card| card.to_string())
-                .collect::<Vec<String>>()
-                .join(", ")
-                + format!(
-                    " ({} points)",
-                    if self.points != 0 {
-                        self.points.to_string()
-                    } else {
-                        "> 21".to_string()
-                    }
-                )
-                .as_str()
+            "{} and {} ({} points)",
+            elements.join(", "),
+            last,
+            if points > 0 {
+                points.to_string()
+            } else {
+                String::from("> 21")
+            }
         )
     }
 }
 
 impl Hand {
+    /// Creates a new instance of a `Hand` given a `Deck` to get the initial cards.
     pub fn new(deck: &mut Deck) -> Hand {
         let cards: Vec<Card> = deck.get_initial_cards();
         let points: u8 = Hand::calculate_points(&cards);
@@ -47,14 +47,32 @@ impl Hand {
         hand.check_ace_points();
         hand
     }
+    /// Creates a new instance of a `Hand` given a slice of `Card`s
+    pub fn from_cards(cards: &[Card]) -> Hand {
+        let points: u8 = Hand::calculate_points(cards);
+
+        let mut hand = Hand {
+            cards: cards.into(),
+            points,
+            aces: 0,
+        };
+        for card in hand.cards.clone() {
+            hand.check_if_ace(&card);
+        }
+        hand.check_ace_points();
+        hand
+    }
 
     pub fn has_blackjack(&self) -> bool {
         self.cards.len() == 2 && self.points == 21
     }
+
+    /// Resets all the attributes of the instance.
     pub fn initialize_attributes(&mut self, deck: &mut Deck) {
         *self = Hand::new(deck);
     }
 
+    /// Deal a new `Card`, taken from the `Deck` given as a parameter.
     pub fn deal_card(&mut self, deck: &mut Deck) {
         let card: Card = deck.deal_card();
         self.check_if_ace(&card);
