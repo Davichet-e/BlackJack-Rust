2a9a532fa6b643450810101740c1f56743e5e0fa
David Garcia <davidgmorillop@gmail.com>
2020-08-05 12:55:59 +0200

    started making changes

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # BlackJack-Rust
+
 The 21 BlackJack, now in Rust.
-Up to *7* players.
+Up to _7_ players.
 You can hit, stand, double, split, surrender,
 just like in a real casino.
+Branch in development, do not use it. Still a plenty of errors :(
 
 # Requeriments
+
 You must have Rust installed in your computer, (see <a href="https://www.rust-lang.org/tools/install">Rust installation steps</a>)
 
 # Run it
-To run this application, run `cargo run` in your terminal or download it from <a href="https://github.com/Davichet-e/BlackJack-Rust/releases">here</a> and launch the executable
 
+To run this application, run `cargo run` in your terminal or download it from <a href="https://github.com/Davichet-e/BlackJack-Rust/releases">here</a> and launch the executable
--- src/player.rs
+++ src/player.rs
@@ -4,12 +4,18 @@ use crate::hand::Hand;
 use std::fmt;
 
 #[derive(Clone)]
+pub struct HandWrap {
+    hand: Hand,
+    splitted_hand: Option<Hand>,
+}
+
+#[derive(Clone)]
 pub struct Player {
-    pub hands: Vec<Hand>,
-    pub name: String,
-    pub initial_money: u32,
-    pub actual_money: u32,
-    pub bet: u32,
+    hand: HandWrap,
+    name: String,
+    initial_money: u32,
+    actual_money: u32,
+    bet: u32,
 }
 
 impl fmt::Display for Player {
@@ -24,36 +30,75 @@ impl Player {
     /// and a `deck` to create the `hands` field
     pub fn new(name: String, initial_money: u32, deck: &mut Deck) -> Player {
         Player {
-            hands: vec![Hand::new(deck)],
+            hand: HandWrap {
+                hand: Hand::new(deck),
+                splitted_hand: None,
+            },
             name,
             initial_money,
             actual_money: initial_money,
             bet: 0,
         }
     }
-    /// Reset the attributes of the `Hand`s of the instance.
-    pub fn reset_hands(&mut self, deck: &mut Deck) {
-        self.hands
-            .iter_mut()
-            .for_each(|hand| hand.initialize_attributes(deck))
+    pub fn hand(&self) -> &HandWrap {
+        &self.hand
+    }
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+    pub fn initial_money(&self) -> u32 {
+        self.initial_money
+    }
+    pub fn actual_money(&self) -> u32 {
+        self.actual_money
+    }
+    pub fn bet(&self) -> u32 {
+        self.bet
     }
 
-    pub fn bet(&mut self, money: u32) {
+    pub fn hand_mut(&mut self) -> &mut HandWrap {
+        &mut self.hand
+    }
+    pub fn name_mut(&mut self) -> &mut String {
+        &mut self.name
+    }
+    pub fn initial_money_mut(&mut self) -> &mut u32 {
+        &mut self.initial_money
+    }
+    pub fn actual_money_mut(&mut self) -> &mut u32 {
+        &mut self.actual_money
+    }
+    pub fn bet_mut(&mut self) -> &mut u32 {
+        &mut self.bet
+    }
+    /// Reset the attributes of the `Hand`s of the instance.
+    pub fn reset_hands(&mut self, deck: &mut Deck) {
+        self.hand.hand.initialize_attributes(deck);
+        if let Some(hand) = self.hand.splitted_hand {
+            hand.initialize_attributes(deck);
+        }
+    }
+
+    pub fn make_bet(&mut self, money: u32) {
         self.bet = money;
     }
 
-    pub fn hit(&mut self, deck: &mut Deck, hand_index: usize) {
-        self.hands[hand_index].deal_card(deck)
+    pub fn hit(&mut self, deck: &mut Deck, splitted_hand: bool) {
+        if splitted_hand {
+            self.hand.splitted_hand.unwrap().deal_card(deck);
+        } else {
+            self.hand.hand.deal_card(deck);
+        }
     }
 
     /// Double the player's bet if applicable, return an error message otherwise
     pub fn double(&mut self) -> Option<&str> {
         if self.bet * 2 > self.actual_money {
             Some("Cannot double because you have not enough money!")
-        } else if self.hands[0].cards.len() > 2 {
+        } else if self.hand.hand.cards.len() > 2 {
             Some("Cannot double because you have already hit!")
         } else {
-            if self.hands.get(1).is_some() {
+            if self.hand.splitted_hand.is_some() {
                 self.bet += self.bet / 2;
             } else {
                 self.bet *= 2;
@@ -65,24 +110,24 @@ impl Player {
     /// Perform the corresponding operations to the player's surrender if applicable,
     /// return an error message otherwise
     pub fn surrender(&mut self) -> Option<&str> {
-        if self.hands[0].cards.len() != 2 {
+        if self.hand.hand.cards.len() != 2 {
             Some("Cannot surrender because you have already hit!")
-        } else if self.hands.get(1).is_some() {
+        } else if self.hand.splitted_hand.is_some() {
             Some("Cannot surrender because you have already splitted!")
         } else {
             self.bet /= 2;
-            self.hands[0].points = 0;
+            self.hand.hand.points = 0;
             None
         }
     }
 
     /// Splits the player's hand if applicable, return an error message otherwise
     pub fn split(&mut self, deck: &mut Deck) -> Option<&str> {
-        let first_hand_cards: &Vec<Card> = &self.hands[0].cards;
+        let first_hand_cards: &Vec<Card> = &self.hand.hand.cards;
 
         if self.bet * 2 > self.actual_money {
             Some("Cannot split because you have not enough money!")
-        } else if self.hands.get(1).is_some() {
+        } else if self.hand.splitted_hand.is_some() {
             Some("Cannot split because you have already splitted!")
         } else if first_hand_cards.len() != 2 {
             Some("Cannot split because you have already hit!")
@@ -91,24 +136,30 @@ impl Player {
         } else {
             self.bet *= 2;
             let cards: Vec<Card> = vec![
-                self.hands[0].cards.pop().expect("Failed to split"),
+                self.hand.hand.cards.pop().expect("Failed to split"),
                 deck.deal_card(),
             ];
 
-            self.hands.push(Hand::from_cards(&cards));
+            self.hand.splitted_hand = Some(Hand::from_cards(&cards));
 
-            self.hands[0].deal_card(deck);
+            self.hand.hand.deal_card(deck);
             None
         }
     }
 
     /// Perform the corresponding operations with the player's money,
     /// return the amount of money the player wins
-    pub fn win(&mut self, hand_index: usize) -> u32 {
+    pub fn win(&mut self, splitted_hand: bool) -> u32 {
         let money_before = self.actual_money;
-        if self.hands.len() == 1 {
+        if self.hand.splitted_hand.is_none() {
             self.actual_money += self.bet;
-            if self.hands[hand_index].has_blackjack() {
+            if !splitted_hand && self.hand.hand.has_blackjack()
+                || splitted_hand
+                    && self
+                        .hand
+                        .splitted_hand
+                        .map_or(false, |hand| hand.has_blackjack())
+            {
                 self.actual_money += self.bet / 2;
             }
         } else {
